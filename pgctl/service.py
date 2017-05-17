4164a9190c9a8e71578ed0ebfdf9c765be25bd8f
Noah Gold <ngold@yelp.com>
2017-05-16 18:19:16 -0700

    More fixes.

--- pgctl/service.py
+++ pgctl/service.py
@@ -111,10 +111,13 @@ class Service(namedtuple('Service', ['path', 'scratch_dir', 'default_timeout']))
         """Idempotent stop of a service or group of services"""
         self.ensure_exists()
         # TODO: ensure_logs
-        svc(('-dx', self.path.strpath))
-
-        if not with_log_running:
-            svc(('-dx', self.path.join('log').strpath))
+        try:
+            svc(('-dx', self.path.strpath))
+        except Exception as e:
+            raise e
+        finally:
+            if not with_log_running:
+                svc(('-dx', self.path.join('log').strpath))
 
     def force_cleanup(self):
         """Forcefully stop a service (i.e., `kill -9` all processes locking on `self.path.strpath`)"""
